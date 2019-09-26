55fb6f9879c132a9cd08e9cacc51d24ed311f563
LiterallyAKing <alexander.king@nyu.edu>
2019-09-26 16:45:37 -0500

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+public Transform target;
+Vector3 offsetVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetVector = this.transform.position - target.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.LookAt(target);
+        Vector3 newpos = target.position + offsetVector;
+        //newpos.y = this.transform.position.y;
+        //newpos.z = this.transform.position.z;
+
+        Vector3 moveVector = this.transform.position - newpos;
+        moveVector *= 0.25f;
+        newpos += moveVector;
+        this.transform.position = newpos;
+
+        //this.transform.position = Vector3.MoveTowards(this.transform.position, newpos, 0.01f);
+    }
+}
---
+++ Assets/FadeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    Image myimage;
+    public bool fadeIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+     myimage = GetComponent<Image>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fadeIn){
+            Color newcolor = myimage.color;
+
+            newcolor.a = Mathf.Lerp(1f,0,Time.time / 1f);
+            myimage.color = newcolor;
+
+        }
+    }
+}
